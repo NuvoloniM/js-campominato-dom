48f0c73ec3d66a696c10e043bd721368ebe2c830
NuvoloniM <nuvoloni.m@gmail.com>
2022-03-22 16:34:58 +0100

    Classe "bombe"

--- asset/app.js
+++ asset/app.js
@@ -17,3 +17,119 @@ Le bombe dovranno essere generate nello stesso range delle caselle di gioco.
 Consigli del giorno: :party_wizard:
 Scriviamo prima cosa vogliamo fare passo passo in italiano, dividiamo il lavoro in micro problemi. Ad esempio: Di cosa ho bisogno per generare i numeri? Proviamo sempre prima con dei console.log() per capire se stiamo ricevendo i dati giusti. Le validazioni e i controlli possiamo farli anche in un secondo momento.
 */
+
+
+//Variabili Globali
+let btn = document.getElementById('btn');
+let btnReset = document.getElementById('btn_reset');
+
+let diff = document.getElementById('difficult');
+let container = document.getElementById('container');
+let min = 1;
+let max = 100;
+
+//eventi 
+btn.addEventListener('click', play);
+btnReset.addEventListener('click',reset)
+
+
+function play() {
+    //Reset del contenuto interno per non far accumulare più griglie
+    container.innerHTML = '';
+    //Nascondi il placeholder o home di gioco 
+    let placeholder = document.getElementById('placeholder');
+    placeholder.className = 'd-none';
+    //fai apparire la griglia
+    container.classList.remove('d-none');
+    let diff = document.getElementById('difficult');
+    let x = diff.value;
+    let array = [];
+    
+    if (x == 'hard') {
+        document.documentElement.style.setProperty('--column', '7');
+        max = 49;
+    } else if (x == 'normal'){
+        document.documentElement.style.setProperty('--column', '9');
+        max = 81;
+    } else {
+        document.documentElement.style.setProperty('--column', '10');
+        max = 100;
+    }
+    //creo array di numeri da un min e un max a seconda della difficoltà
+    array = arrayCreation(min,max);
+    //mischio gli elementi dell'array per non stampare numeri sequenziali
+    array = shuffleArray(array);
+    console.log(array)
+    //creo un array bombe lungo quanto le celle ed estraggo i primi 16 numeri
+    let bombe = shuffleArray(arrayCreation(min,max));
+    bombe = sixteen(bombe);
+    console.log(bombe)
+    for (let i = 0; i < max; i++) {
+        //a ogni giro creo un div
+        let card = document.createElement('div');
+        //ad ogni div assegno la classe css creata
+        card.classList.add('card');
+        //in ogni div stampo un numero casuale tra min e max
+        // let casualN = randomInt(min,max); 
+        card.innerHTML += `${array[i]}`;
+        //appendo il div creato al container
+        container.appendChild(card);
+    
+        //creo evento
+        card.addEventListener('click',function () {
+            console.log(this.innerHTML);
+            if (bombe.includes(Number(this.innerHTML)) == true) {
+                this.classList.add('bomb');
+            } else {
+                this.classList.add('selected');
+            }
+        });
+    }
+    
+}
+
+
+
+
+//Funzioni Generali
+
+//creo array da min a max 
+function arrayCreation(min,max) {
+    let array = []; 
+    for (let i = min; i <= max; i++) {
+        array.push(i);
+    }
+    return array
+ }
+
+ //funzione shuffle (Funzione che mischia gli indici di un array)
+function shuffleArray(array){
+    array.sort(()=> Math.random() - 0.5);
+    return array
+}
+
+//creo funzione che assegna all'elemento selezionato la classe selected 
+// function selected(array,x) {
+    //funzione che, collegata ad un evento, cambia classe all'elemento(this) cliccato
+    //condizione per vedere quale classe aggiungere
+    // if (array.includes(x) == false) {
+//        this.classList.add('selected'); 
+//     } else {
+//         this.classList.add('bomb');
+//     }
+    
+// }
+
+//funzione reset
+function reset() {
+    return history.go(0);
+}
+
+//prendi le prime 16 i di un array
+function sixteen(array) {
+    let array2 = [];
+    for (let i = 0; i < 16; i++) {
+        array2.push(array[i]);
+    }
+    return array2;
+}
